723322aa63e6fe9ac254adf620d2e7acaa385a97
gilangfatahilah <gilangf010@gmail.com>
2024-11-26 23:41:39 +0700

    🌈 style(CTA section): add animation

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -1,15 +1,43 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+
 const CallToAction = () => {
+  const [scope, animate] = useAnimate();
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const animation = useRef<AnimationPlaybackControls>();
+
+  useEffect(() => {
+    animation.current = animate(
+      scope.current,
+      { x: "-50%" },
+      { duration: 30, ease: "linear", repeat: Infinity }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (animation.current) {
+      animation.current.speed = isHovered ? 0.5 : 1;
+    }
+  }, [isHovered]);
+
   return (
     <section className="py-24">
       <div className="overflow-x-clip p-4 flex">
-        <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+        <motion.div
+          ref={scope}
+          className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium cursor-pointer group"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           {Array.from({ length: 10 }).map((_, index) => (
             <div key={index} className="flex items-center gap-16">
               <span className="text-lime-400 text-7xl">&#10038;</span>
-              <span>Try it for free</span>
+              <span className="group-hover:text-lime-400">Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
